1de37aaebd83ccefb48e7c1eaa2afb474c2371df
Hamid Khaldi <hamidkhaldi@aol.com>
2017-04-18 18:40:47 +0100

    Add files via upload

---
+++ walking_project/script.js
@@ -0,0 +1,104 @@
+/**
+ * Created by Hamid on 13/03/2017.
+ * @name    script.js
+ * @desc    this script will animate a walking sprite
+ */
+
+(function(){
+
+    var
+
+        i                   =   0,
+        leftMargin          =   0,
+        path                =   "images/walking_sprite_2/",
+        picArrayRight       =   ["right0.jpg", "right1.jpg", "right2.jpg", "right3.jpg",
+                                "right4.jpg", "right5.jpg", "right6.jpg"],
+        picArrayLeft        =   ["left0.jpg", "left1.jpg", "left2.jpg", "left3.jpg",
+                                "left4.jpg", "left5.jpg", "left6.jpg"],
+        walker              =   document.getElementById("walker"),
+        startBtn            =   document.getElementById("startBtn"),
+        stopBtn             =   document.getElementById("stopBtn"),
+        walkTimeOut,
+
+
+
+        walkRight = function () {
+
+                if (leftMargin < 350) {
+
+                    if (i < picArrayRight.length) {
+                        leftMargin += 10;
+                        walker.src = path + picArrayRight[i];
+                        walker.style.marginLeft = leftMargin + "px";
+
+                        i++;
+
+                    } else {
+                        i = 0;
+                    }
+                    walkTimeOut = setTimeout(function () {
+                        walkRight();
+                    }, 100);
+
+                } else {
+                    walkLeft();
+                }
+            },//walkRight
+
+            walkLeft = function () {
+                if (leftMargin > 0) {
+                    if (i < picArrayLeft.length) {
+                        leftMargin -= 10;
+                        walker.style.marginLeft = leftMargin + "px";
+                        walker.src = path + picArrayLeft[i];
+                        i++;
+
+                    } else {
+                        i = 0;
+                    }
+                    walkTimeOut = setTimeout(function () {
+                        walkLeft();
+                    }, 100);
+                } else {
+                    walkRight();
+                }
+
+            },//walkLeft
+
+
+            stopWalk  =   function(){
+                clearTimeout(walkTimeOut);
+
+
+            }//stopWalk
+
+            /*walk    =   function (){
+                if (picArrayRight.includes())
+
+            }//walk*/
+
+
+       bindBtns    =   function(){
+
+                    startBtn.addEventListener("click", function () {
+                        walkRight();
+                        startBtn.disabled = true;
+                        stopBtn.disabled = false;
+
+                    });
+                    stopBtn.addEventListener("click", function(){
+                        stopWalk();
+                        startBtn.disabled = false;
+                        stopBtn.disabled = true;
+                    });
+
+        },//bindBtns
+
+
+        init = function () {
+                bindBtns();
+
+        };//init
+
+    window.addEventListener("load",init);
+})();
